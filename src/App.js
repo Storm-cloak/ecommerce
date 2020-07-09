50922464bb78cd8902ac9df7fcd218ec6232e3f7
Storm-cloak <cboocy@mail.ru>
2020-07-10 01:46:03 +0400

    added shop page

--- src/App.js
+++ src/App.js
@@ -1,14 +1,18 @@
 import React from "react";
 import HomePage from "./pages/homepage/homepage.component";
+import ShopPage from "./pages/shop/shop.component";
 import "./App.css";
-class App extends React.Component {
-  render() {
-    return (
-      <div>
-        <HomePage />
-      </div>
-    );
-  }
+import { Switch, Route } from "react-router-dom";
+
+function App() {
+  return (
+    <div>
+      <Switch>
+        <Route exact path="/" component={HomePage}></Route>
+        <Route exact path="/shop" component={ShopPage}></Route>
+      </Switch>
+    </div>
+  );
 }
 
 export default App;
