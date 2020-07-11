9a7b7a07bbec63191d50fff015d0987ede14c028
Storm-cloak <cboocy@mail.ru>
2020-07-11 22:00:05 +0400

    added Header, links to shop,home,contact pages

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React from "react";
+import Header from "./components/header/header.component.jsx";
 import HomePage from "./pages/homepage/homepage.component";
 import ShopPage from "./pages/shop/shop.component";
 import "./App.css";
@@ -7,6 +8,7 @@ import { Switch, Route } from "react-router-dom";
 function App() {
   return (
     <div>
+      <Header />
       <Switch>
         <Route exact path="/" component={HomePage}></Route>
         <Route exact path="/shop" component={ShopPage}></Route>
