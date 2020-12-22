01a3676c33bc0481fc4ecce57dcc8682d097a953
Storm-cloak <raghamoghlanov@gmail.com>
2020-12-22 16:46:21 +0400

    Configurated server for stripe payments

--- ecommerce-front/src/components/stripe-button/stripe-button.component.jsx
+++ ecommerce-front/src/components/stripe-button/stripe-button.component.jsx
@@ -1,13 +1,28 @@
 import React from "react";
 import StripeCheckout from "react-stripe-checkout";
-
+import axios from "axios";
 const StripeCheckoutButton = ({ price }) => {
   const priceForStripe = price * 100;
   const publishableKey =
     "pk_test_51HAI24I70T80NG68qWgiimImv676S6lMlahLah8YYLhHoJ4IJjWG2ZeXltJ1hbZeu0POghmN4VP94giJVr6I2LrV005tWgXEVm";
   const onToken = (token) => {
-    alert("Payment successfull");
+    axios({
+      url: "payment",
+      method: "post",
+      data: {
+        amount: priceForStripe,
+        token: token,
+      },
+    })
+      .then((response) => {
+        alert("payment success");
+      })
+      .catch((error) => {
+        console.log("payment error", error);
+        alert("There was an issue with payment");
+      });
   };
+
   return (
     <StripeCheckout
       label="Pay Now"
